e66f85e2afeb8840ed9109c9d326ca7cc1ee6179
Selina <31773266+selinagerosa@users.noreply.github.com>
2019-04-09 18:49:46 -0400

    adds alt text to all pages, adds secret mystery button

---
+++ functions.js
@@ -0,0 +1,2 @@
+function randfriend(){
+  var e=["https://derichamer.me/","http://gelard.com","http://www.herger.me/","http://q-mart.net/","https://www.danielledinstman.com/","https://www.gracecolbertdesign.com/","https://www.tabithamichaelidis.com/"],n=Math.floor(Math.random()*e.length);window.location.href=e[n]}
